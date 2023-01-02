3817619865de170c2682bc68f2a5c243320f5742
ckester99 <christiankester77@gmail.com>
2023-01-02 17:45:58 -0500

    fix port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const util = require("util");
 const uniqid = require("uniqid");
 const app = express();
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
