3e82fd1c0d380ff331634ec1af5ca43cef29071d
Christian Kester <christiankester77@gmail.com>
2023-01-02 12:20:58 -0500

    added post method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("node:path");
 const fs = require("fs");
 const util = require("util");
+const uniqid = require("uniqid");
 const app = express();
 const port = 3001;
 
@@ -10,6 +11,44 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const readFromFile = util.promisify(fs.readFile);
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+const readAndAppend = (content, file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+app.post("/api/notes", (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newFeedback = {
+            title,
+            text,
+            id: uniqid(),
+        };
+
+        readAndAppend(newFeedback, "./db/db.json");
+
+        const response = {
+            status: "success",
+            body: newFeedback,
+        };
+        console.log(`Post Response: ${response}`);
+        res.json(response);
+    } else {
+        res.json("Error in posting feedback");
+    }
+});
 
 app.get("/", (req, res) => {
     res.sendFile("./public/index.html");
